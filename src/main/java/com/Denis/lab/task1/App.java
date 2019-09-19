417f4001222ce7d7c0ac6cddb14c042c2ee2e9ca
DKLigos <Zubkodenis08@gmail.com>
2019-09-19 08:04:55 +0300

    try

---
+++ src/main/java/com/Denis/lab/task1/App.java
@@ -0,0 +1,7 @@
+package com.Denis.lab.task1;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/Denis/lab/task1/Book.java
@@ -0,0 +1,16 @@
+package com.Denis.lab.task1;
+
+public class Book {
+    public static void main(String[] args) {
+        String name = "Александр";
+        String book = "Образование";
+        int ags = 1984;
+        String c = (test("Саша"));
+        System.out.println(c);
+    }
+  public static String test(String test1){
+      String name="Петр";
+      return name;
+
+  }
+}
